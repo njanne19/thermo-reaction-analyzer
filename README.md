438ac40077e73a5b67d9d5e97c5e405dbceae358
Nick Janne <nickjanne7@gmail.com>
2017-05-26 11:08:14 -0400

    Final touches and ready to go

---
+++ README.md
@@ -0,0 +1,11 @@
+# Instructions on how to use the project
+
+## use the form: PRODUCTS = REACTANTS <br>
+YOU MUST ADD a (form of matter) WITH EVERY COMPOUND (e.g. Fe(s))
+
+Separate both sides within compounds with a "plus sign (+)". Include coefficients at the front of each compound. This project does not check for a balanced chemical equation.
+
+This project is based off of two API's I created. They return JSON data and can be found here, and used on their own.
+
+<a href ="https://github.com/njanne19/entropy-API">Entropy API</a>
+<a href ="https://github.com/njanne19/enthalpy-calculator">Enthalpy API</a>
--- index.js
+++ index.js
@@ -182,7 +182,7 @@ $(document).ready(function () {
         }
         if (temp == 298) {
             var gibbsLocal = enthalpy - 298*(entropy/1000);
-            $('#therm').append("<h3>" + "∆Gº @ 298K" + gibbsLocal + "</h3>"); 
+            $('#therm').append("<h3>" + "∆Gº @ 298K" + gibbsLocal.toFixed(2) + "</h3>"); 
         } else {
             var gibbsLocal25 = enthalpy - 298*(entropy/1000);
             var gibbsLocalNew = enthalpy - temp*(entropy/1000);
@@ -192,19 +192,19 @@ $(document).ready(function () {
         } 
             var critical = enthalpy/(entropy/1000);
             if (enthalpy > 0 && entropy < 0) {
-                $('#spon').append("<h3>" + "Never spontaneous regardless of temperature" + "<h3>");
+                $('#spon').append("<h3>" + "Never spontaneous" + "<br>" + "regardless of temperature" + "<h3>");
             } else if (enthalpy > 0 && entropy > 0) {
                if (temp < critical) {
-                   $('#spon').append("<h3>" + "Not spontaneous at current temperature (" + temp + ") " + "<h3>")
+                   $('#spon').append("<h3>" + "Not spontaneous at current" + "<br>" + "temperature (" + temp + ") " + "<h3>")
                }
-               $('#spon').append("<h3>" + "Always spontaneous above: " + critical.toFixed(2) + "ºK" + "<h3>");
+               $('#spon').append("<h3>" + "Always spontaneous" + "<br>" + "above: " + critical.toFixed(2) + "ºK" + "<h3>");
             } else if (enthalpy < 0 && entropy > 0) {
-                $('#spon').append("<h3>" + "Always spontaneous regardless of temperature" + "<h3>")
+                $('#spon').append("<h3>" + "Always spontaneous" + "<br>" + "regardless of temperature" + "<h3>")
             } else if (enthalpy < 0 && entropy < 0) {
                if (temp < critical) {
-                   $('#spon').append("<h3>" + "Spontaneous at current temperature (" + temp + "Kº) " + "<h3>")
+                   $('#spon').append("<h3>" + "Spontaneous at current" + "<br>" + "temperature (" + temp + "Kº) " + "<h3>")
                }
-               $('#spon').append("<h3>" + "Always spontaneous below: " + critical.toFixed(2) + "ºK" + "<h3>");
+               $('#spon').append("<h3>" + "Always spontaneous" + "<br>" + "below: " + critical.toFixed(2) + "ºK" + "<h3>");
             }
         
         
@@ -212,6 +212,7 @@ $(document).ready(function () {
         
 	});
 
+    
 
     
     
